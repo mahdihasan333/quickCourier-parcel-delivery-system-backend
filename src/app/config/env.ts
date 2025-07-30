c06e8c6cc27fee47686cf9eb134c1b1f1dfcda71
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-30 20:57:42 +0600

    update

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,31 +1,40 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 interface EnvConfig {
-    PORT: string,
-    DB_URL: string,
-    NODE_ENV: "development" | "production"
-    
-
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: 'development' | 'production';
+  BCRYPT_SALT_ROUND: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"];
+  const requiredEnvVariables: string[] = [
+    'PORT',
+    'DB_URL',
+    'NODE_ENV',
+    'BCRYPT_SALT_ROUND',
+    'JWT_ACCESS_SECRET',
+    'JWT_ACCESS_EXPIRES',
+  ];
 
-    requiredEnvVariables.forEach(key => {
-        if (!process.env[key]) {
-            throw new Error(`Missing require environment variabl ${key}`)
-        }
-    })
-
-    return {
-        PORT: process.env.PORT as string,
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        DB_URL: process.env.DB_URL!,
-        NODE_ENV: process.env.NODE_ENV as "development" | "production",
-        
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing required environment variable ${key}`);
     }
-}
+  });
 
-export const envVars = loadEnvVariables()
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL as string,
+    NODE_ENV: process.env.NODE_ENV as 'development' | 'production',
+    BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+  };
+};
+
+export const envVars = loadEnvVariables();
